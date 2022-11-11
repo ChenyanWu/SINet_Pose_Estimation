04f15813ba44666920e831fb04815b8565326b9a
chenyanwu <czw390@psu.edu>
2022-11-11 02:32:22 -0500

    .

---
+++ lib/models/__init__.py
@@ -0,0 +1,25 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.pose_resnet
+import models.pose_hrnet
+import models.nas_hrnet
+import models.covariance_hrnet
+import models.covariance_hrnet_v2
+import models.covariance_hrnet_v3
+import models.covariance_hrnet_v4
+import models.covariance_hrnet_v5
+import models.covariance_hrnet_v6
+import models.covariance_hrnet_v7
+import models.sync_batchnorm
